4436f70abe7d88dfa4291a53e2c373393c4f6093
Roi Doron <roidoron6@gmail.com>
2024-04-01 21:57:22 +0300

    Refactor and new build

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,5 @@
-export default {
-    dbURL: 'mongodb://127.0.0.1:27017',
-    dbName: 'toy_db',
+export default  {
+  // dbURL: 'mongodb://127.0.0.1:27017',
+  dbURL: 'mongodb+srv://roidoron:1d2o3r4o5n@toy.so71adh.mongodb.net/?retryWrites=true&w=majority&appName=toy',
+  dbName : 'toy_db'
 }
