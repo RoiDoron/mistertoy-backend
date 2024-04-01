b65c2ed396fd1e2a907b6ae26b18350e1faab72e
Roi Doron <roidoron6@gmail.com>
2024-04-01 22:02:51 +0300

    Fix bug

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-  dbName : process.env.DB_NAME || 'tester_db'
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://roidoron:1d2o3r4o5n@toy.so71adh.mongodb.net/?retryWrites=true&w=majority&appName=toy',
+  dbName : process.env.DB_NAME || 'toy_db'
 }
